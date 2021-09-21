a03d5d1b45b9db3d89f78aa5da66977871bfb2cb
Nicolas Bagnath <nicolas.bagnath@gmail.com>
2021-09-21 20:02:04 +0400

    Stupid Coaching

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,15 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:answer]
+      if @question == "I am going to work right now!"
+        return @answer = "Great!"
+      elsif @question.include?("?")
+        return @answer = "Silly question, get dressed and go to work!"
+      else
+        return @answer = "I don't care, get dressed and go to work!"
+      end
+  end
+end
